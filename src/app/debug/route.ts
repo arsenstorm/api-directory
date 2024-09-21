402c428defd0df1426c0227848e983f83a0128a5
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-21 12:50:32 +0100

    remove debug route

--- src/app/debug/route.ts
+++
@@ -1,8 +0,0 @@
-import { getConfig } from "@/utils/get-config";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const config = await getConfig();
-
-  return NextResponse.json(config);
-}
