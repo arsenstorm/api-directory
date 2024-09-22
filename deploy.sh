bddaea04ed6320b80b39188079099b1e41de3d5b
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-22 13:15:45 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # Run docker-compose up
 docker-compose up --build --force-recreate -d
 
-# Deploy using kamal-proxy with NEXT_PUBLIC_SITE_URL from .env with TLS
-docker-compose exec proxy kamal-proxy rollout deploy main --target request-directory:3000 --host ${NEXT_PUBLIC_SITE_URL} --tls
+# Deploy with TLS
+docker-compose exec proxy kamal-proxy deploy main --target request-directory:3000 --host ${NEXT_PUBLIC_SITE_URL} --tls
