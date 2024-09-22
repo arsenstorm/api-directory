ffefa866475abf401e20389913835599a2ad5b92
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-22 11:47:07 +0100

    bug fix maybe?

--- setup.sh
+++ setup.sh
@@ -152,8 +152,8 @@ docker_compose['services']['request-directory'] = {
 docker_compose['services']['proxy'] = {
     'image': 'basecamp/kamal-proxy',
     'ports': [
-        '127.0.0.1:80:80',
-        '127.0.0.1:443:443',
+        '80:80',
+        '443:443',
     ],
     'restart': 'unless-stopped',
     'depends_on': ['request-directory'],
