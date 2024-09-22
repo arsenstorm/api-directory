40466f7d93282f7066e111707d7391f42168f1a5
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-22 11:54:06 +0100

    add tls

--- setup.sh
+++ setup.sh
@@ -296,5 +296,5 @@ EOF
 # Run docker-compose up
 docker-compose up --build -d
 
-# Deploy using kamal-proxy with NEXT_PUBLIC_SITE_URL from .env
-#docker-compose exec proxy kamal-proxy deploy main --target request-directory:3000 --host "${NEXT_PUBLIC_SITE_URL}"
+# Deploy using kamal-proxy with NEXT_PUBLIC_SITE_URL from .env with TLS
+docker-compose exec proxy kamal-proxy deploy main --target request-directory:3000 --host ${NEXT_PUBLIC_SITE_URL} --tls
