94bc4fec13cdbc9803a9ca603918974f19295f5d
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-23 21:35:20 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -45,7 +45,7 @@ echo "Deploying to: ${CLEAN_URL}"
 # Run docker-compose up
 if [ "$NO_CACHE" = true ]; then
     echo "Running docker-compose up with --no-cache"
-    docker-compose build --no-cache
+    docker-compose build --no-cache --pull
     docker-compose up --force-recreate -d
 else
     echo "Running docker-compose up with cache"
