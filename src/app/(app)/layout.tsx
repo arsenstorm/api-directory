c2bc2ced930e8d9d3956b2560143121ce6cd4707
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-27 10:59:40 +0100

    Add Supabase Hackathon Banner

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,6 +8,7 @@ import Footer from "@/components/footer";
 import { getConfig } from "@/utils/get-config";
 
 import { Toaster } from "sonner";
+import Banner from "@/components/banner";
 
 export const metadata: Metadata = {
 	title: "Request Directory",
@@ -30,12 +31,13 @@ export default async function RootLayout({
 					href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
 				/>
 			</head>
-			<body
-				className={clsx("px-4 bg-neutral-100/50", "min-h-screen flex flex-col")}
-			>
-				<Navbar config={config} />
-				<div className="flex-1">{children}</div>
-				<Footer />
+			<body className={clsx("bg-neutral-100/50", "min-h-screen flex flex-col")}>
+				<Banner />
+				<div className="px-4">
+					<Navbar config={config} />
+					<div className="flex-1">{children}</div>
+					<Footer />
+				</div>
 				<Toaster richColors />
 			</body>
 		</html>
